d51fa5969c0ffee8388cd5b2b825b92a1bdf467c
Babaganoosh76 <amiller5233@gmail.com>
2020-07-13 13:59:04 -0700

    updated wsgi filepath

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,6 @@
 import logging
 import sys
 logging.basicConfig(stream=sys.stderr)
-sys.path.insert(0, '/var/www/personal-website')
+sys.path.insert(0, '/var/www/personal_website')
 
 from personal_website import app as application
