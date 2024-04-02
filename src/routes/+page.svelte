6a5a42448ae55f49d7a2171c4b9724b4c3ebeed6
amiller5233@gmail.com <amiller5233@gmail.com>
2024-04-02 01:09:56 -0700

    cave and use p5-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import P5 from 'p5-svelte';
 
 	import BlockLink from '$lib/components/BlockLink.svelte';
 	import GridLink from '$lib/components/GridLink.svelte';
@@ -8,98 +9,95 @@
 
 	onMount(async () => {
 		import ('$lib/stripe-heading.js');
-		import ('$lib/p5.min.js').then(res => {
+	});
 
-			// p5 instance
-			const sketch = (p5) => {
-				let canvasWidth = p5.windowWidth;
-				let canvasHeight = p5.windowHeight * 1.1;
-				const DOT_SIZE = 8;
-				const DOT_GAP = 40;
+		
+	const sketch = (p5) => {
+		let canvasWidth = p5.windowWidth;
+		let canvasHeight = p5.windowHeight * 1.1;
+		const DOT_SIZE = 8;
+		const DOT_GAP = 40;
 
-				const SPRITE_LAG = 12;
-				let spriteVector = p5.createVector(0,0);
+		const SPRITE_LAG = 12;
+		let spriteVector = p5.createVector(0,0);
 
-				const _calc_size = () => {
-					// get length from center of canvas to the corner
-					let hyp = Math.floor(Math.sqrt(Math.pow(canvasWidth,2) + Math.pow(canvasHeight,2)));
-					// divide by DOT_GAP
-					let exact_size = hyp / DOT_GAP;
-					// round up to nearest odd number (odd needed for symmetrical shape)
-					let rounded_size = 2 * Math.round(Math.ceil(exact_size / 2)) + 1;
-					// calculations don't quite add up, add 1.2x
-					return rounded_size * 1.2;
-				}
-				let SIZE = _calc_size();
+		const _calc_size = () => {
+			// get length from center of canvas to the corner
+			let hyp = Math.floor(Math.sqrt(Math.pow(canvasWidth,2) + Math.pow(canvasHeight,2)));
+			// divide by DOT_GAP
+			let exact_size = hyp / DOT_GAP;
+			// round up to nearest odd number (odd needed for symmetrical shape)
+			let rounded_size = 2 * Math.round(Math.ceil(exact_size / 2)) + 1;
+			// calculations don't quite add up, add 1.2x
+			return rounded_size * 1.2;
+		}
+		let SIZE = _calc_size();
 
-				let debugMode = false;
+		let debugMode = false;
 
 
-				p5.setup = () => {
-					p5.createCanvas(canvasWidth, canvasHeight, document.getElementById('canvas-hero'));
-					p5.fill(0);
-					p5.noStroke();
+		p5.setup = () => {
+			p5.createCanvas(canvasWidth, canvasHeight);
+			p5.fill(0);
+			p5.noStroke();
 
-					spriteVector.set(p5.mouseX, p5.mouseY);
-				};
-				p5.draw = () => {
-					let count = 0;
+			spriteVector.set(p5.mouseX, p5.mouseY);
+		};
+		p5.draw = () => {
+			let count = 0;
 
-					// 1. draw background
-					p5.background(255);
+			// 1. draw background
+			p5.background(255);
 
-					if (debugMode) {
-						p5.fill(180);
-						p5.text(`Mouse: ${p5.mouseX},${p5.mouseY}`, p5.windowWidth - 120, p5.windowHeight - 30);
-						p5.text(`FPS: ${p5.frameRate().toFixed(1)}`, p5.windowWidth - 120, p5.windowHeight - 15);
-					}
+			if (debugMode) {
+				p5.fill(180);
+				p5.text(`Mouse: ${p5.mouseX},${p5.mouseY}`, p5.windowWidth - 120, p5.windowHeight - 30);
+				p5.text(`FPS: ${p5.frameRate().toFixed(1)}`, p5.windowWidth - 120, p5.windowHeight - 15);
+			}
 
-					let xOrigin = (p5.windowWidth/2);
-					let yOrigin = (p5.windowHeight/2);					
+			let xOrigin = (p5.windowWidth/2);
+			let yOrigin = (p5.windowHeight/2);					
 
-					// 2. update sprite
-					spriteVector.add((p5.mouseX - spriteVector.x)/SPRITE_LAG, (p5.mouseY - spriteVector.y)/SPRITE_LAG);
-					if (debugMode) {
-						p5.fill(255, 200, 200);
-						p5.circle(spriteVector.x, spriteVector.y, 24);
-					}
+			// 2. update sprite
+			spriteVector.add((p5.mouseX - spriteVector.x)/SPRITE_LAG, (p5.mouseY - spriteVector.y)/SPRITE_LAG);
+			if (debugMode) {
+				p5.fill(255, 200, 200);
+				p5.circle(spriteVector.x, spriteVector.y, 24);
+			}
 
-					// 3. draw grid
-					const _draw_node = (vector) => {
-						let x = xOrigin + vector.x;
-						let y = yOrigin + vector.y;
-						let d = p5.dist(spriteVector.x, spriteVector.y, x, y);
-						let d_exp = p5.exp(-0.01 * d);
-						let r = DOT_SIZE * (1 + 3*d_exp);
-						p5.fill(240 - 30*d_exp);
-						p5.circle(x, y, r);
-					}
+			// 3. draw grid
+			const _draw_node = (vector) => {
+				let x = xOrigin + vector.x;
+				let y = yOrigin + vector.y;
+				let d = p5.dist(spriteVector.x, spriteVector.y, x, y);
+				let d_exp = p5.exp(-0.01 * d);
+				let r = DOT_SIZE * (1 + 3*d_exp);
+				p5.fill(240 - 30*d_exp);
+				p5.circle(x, y, r);
+			}
 
-					let num_rows = Math.ceil(SIZE/2)
-					for (let j = 0; j < num_rows; j++) {
+			let num_rows = Math.ceil(SIZE/2)
+			for (let j = 0; j < num_rows; j++) {
 
-						let xRow = (DOT_GAP + j * DOT_GAP * 0.5);
-						let yRow = (j * DOT_GAP * p5.cos(p5.PI/6));
+				let xRow = (DOT_GAP + j * DOT_GAP * 0.5);
+				let yRow = (j * DOT_GAP * p5.cos(p5.PI/6));
 
-						let num_dots = SIZE - j;
-						for (let i = 0; i < num_dots; i++) {
-							let xCol = (i - num_rows) * DOT_GAP + xRow;
+				let num_dots = SIZE - j;
+				for (let i = 0; i < num_dots; i++) {
+					let xCol = (i - num_rows) * DOT_GAP + xRow;
 
-							let angle = p5.millis() / 10000;
-							_draw_node(p5.createVector(xCol, yRow).rotate(angle));
-							_draw_node(p5.createVector(xCol, -yRow).rotate(angle));
-						}
-					}
-				};
-
-				p5.windowResized = () => {
-					p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
-					SIZE = _calc_size();
+					let angle = p5.millis() / 10000;
+					_draw_node(p5.createVector(xCol, yRow).rotate(angle));
+					_draw_node(p5.createVector(xCol, -yRow).rotate(angle));
 				}
 			}
-			new p5(sketch);
-		});
-	});
+		};
+
+		p5.windowResized = () => {
+			p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+			SIZE = _calc_size();
+		}
+	}
 </script>
 
 <section class="hero-container">
@@ -123,7 +121,8 @@
 			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/></svg>
 		</div>
 	</div>
-	<canvas id="canvas-hero" class="canvas-pane"></canvas>
+	<!-- <canvas id="canvas-hero" class="canvas-pane"></canvas> -->
+	<P5 {sketch} class="test" />
 	<div class="canvas-pane-fade"></div>
 </section>
 
